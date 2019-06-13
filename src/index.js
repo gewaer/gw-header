0708dd1bb06a1da35d6840751ad0d8d8c7d33035
SparoHawk <sparohawk@gmail.com>
2019-06-13 16:25:16 -0400

    Fixing impor typo

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import GwCompaniesSwitcher from "./components/gw-companies-switcher";
 import GwCompanyLogo from "./components/gw-company-logo";
 import GwNotifications from "./components/gw-notifications";
 import GwUserOptions from "./components/gw-user-options";
-import GwHeader from "./index";
+import GwHeader from "./components/gw-header";
 
 export {
     GwAppSwitcher,
